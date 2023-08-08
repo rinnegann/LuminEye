1c7e9640aa2bdf5c14e62f85cc0bf36ab5735821
rinnegann <nipunsandamal1997@gmail.com>
2023-08-08 10:36:31 +0200

    Changed Wing Loss

--- LuminEye-Iris-Center-Localization/losses/wing_loss.py
+++ LuminEye-Iris-Center-Localization/losses/wing_loss.py
@@ -1,30 +1,22 @@
+import math
 import torch
-import torch.nn as nn
-import numpy as np
+from torch import nn
 
 
+# torch.log  and math.log is e based
 class WingLoss(nn.Module):
-    def __init__(self, width=5, curvature=0.5):
+    def __init__(self, omega=10, epsilon=2):
         super(WingLoss, self).__init__()
-        self.width = width
-        self.curvature = curvature
-        self.C = self.width - self.width * np.log(1 + self.width / self.curvature)
+        self.omega = omega
+        self.epsilon = epsilon
 
-    def forward(self, prediction, target):
-        diff = target - prediction
-        
-        diff_abs = diff.abs()
-        
-        loss = diff_abs.clone()
-        
-
-        idx_smaller = diff_abs < self.width
-        idx_bigger = diff_abs >= self.width
-        
-        
-        loss[idx_smaller] = self.width * torch.log(1 + diff_abs[idx_smaller] / self.curvature)
-        loss[idx_bigger]  = loss[idx_bigger] - self.C
-        
-        
-    
-        return loss
+    def forward(self, pred, target):
+        y = target
+        y_hat = pred
+        delta_y = (y - y_hat).abs()
+        delta_y1 = delta_y[delta_y < self.omega]
+        delta_y2 = delta_y[delta_y >= self.omega]
+        loss1 = self.omega * torch.log(1 + delta_y1 / self.epsilon)
+        C = self.omega - self.omega * math.log(1 + self.omega / self.epsilon)
+        loss2 = delta_y2 - C
+        return (loss1.sum() + loss2.sum()) / (len(loss1) + len(loss2))
